3bb9fef80f10d0797a9f2dc4895f1d994185470b
adanfrancisco@gmail.com <adanfrancisco@gmail.com>
2018-10-04 15:21:52 -0300

    YA PUEDO INGRESAR DATOS PERSONAS

--- tools/nuevo_registro.php
+++ tools/nuevo_registro.php
@@ -3,7 +3,7 @@
      include "db.php";
 
 if($_POST)
-    {
+    {   $dni=isset($_POST['dni']) ? $_POST['dni'] : null;
         $cuil=isset($_POST['cuil']) ? $_POST['cuil'] : null;
         $apellido=isset($_POST['apellido']) ? $_POST['apellido']  : null;
         $nombre=isset($_POST['nombre']) ? $_POST['nombre']  : null;
@@ -18,8 +18,8 @@ if($_POST)
 //$name = strip_tags($_POST['name1']);
 
 
-$sql="INSERT INTO persona(cuil, apellido, nombre, localidad, domicilio, telefono, celular, email, fech_nac, activo )
-VALUES('$cuil', '$apellido' , '$nombre', $localidad , '$domicilio' , '$telefono_fijo' ,
+$sql="INSERT INTO persona(dni,cuil, apellido, nombre, localidad, domicilio, telefono, celular, email, fech_nac, activo )
+VALUES('$dni','$cuil', '$apellido' , '$nombre', $localidad , '$domicilio' , '$telefono_fijo' ,
  '$telefono_celular' , '$email', '$fecha' , 2)";
 $rs = $db->consulta($sql);
 echo $sql;
